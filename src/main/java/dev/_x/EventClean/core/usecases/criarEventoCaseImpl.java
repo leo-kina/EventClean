7a6e00cd0d1a64a4d243aa8e8dd886c0bd545304
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-28 20:30:43 -0300

    feat: adicioando metodos na camada de usecases buscarEvento

--- src/main/java/dev/_x/EventClean/core/usecases/criarEventoCaseImpl.java
+++ src/main/java/dev/_x/EventClean/core/usecases/criarEventoCaseImpl.java
@@ -1,4 +1,10 @@
 package dev._x.EventClean.core.usecases;
 
+import dev._x.EventClean.core.entities.Event;
+
 public class criarEventoCaseImpl implements criarEventoCase{
+    @Override
+    public Event execute(Event evento) {
+        return null;
+    }
 }
