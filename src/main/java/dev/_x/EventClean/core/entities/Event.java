943b19ab6982b38a47bc14bb4ea88bd7c0802703
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-28 20:29:56 -0300

    refactor: refatorando e adicionando o Id nas entidades

--- src/main/java/dev/_x/EventClean/core/entities/Event.java
+++ src/main/java/dev/_x/EventClean/core/entities/Event.java
@@ -6,6 +6,7 @@ import dev._x.EventClean.core.enums.TipoEvento;
 import java.time.LocalDateTime;
 
 public record Event(
+        Long id,
         String nome,
         String descricao,
         String identificador,
--- src/main/java/dev/_x/EventClean/infra/persistence/EventEntity.java
+++ src/main/java/dev/_x/EventClean/infra/persistence/EventEntity.java
@@ -1,4 +1,27 @@
 package dev._x.EventClean.infra.persistence;
 
+import dev._x.EventClean.core.enums.TipoEvento;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Eventos")
 public class EventEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private String identificador;
+    private LocalDateTime datainicio;
+    private LocalDateTime datafim;
+    private String Local;
+    private String organizador;
+    private TipoEvento tipo;
 }
