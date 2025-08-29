9027a07a53d4d687bf46cdb13872fbf5910e584e
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-28 22:39:35 -0300

    feat: usecase buscar

--- src/main/java/dev/_x/EventClean/core/usecases/buscarEventoCaseImpl.java
+++ src/main/java/dev/_x/EventClean/core/usecases/buscarEventoCaseImpl.java
@@ -1,4 +1,10 @@
 package dev._x.EventClean.core.usecases;
 
+import dev._x.EventClean.core.entities.Event;
+
 public class buscarEventoCaseImpl implements buscarEventoCase{
+    @Override
+    public Event execute(Event event) {
+        return null;
+    }
 }
