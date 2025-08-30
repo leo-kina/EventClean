d7472353706fb66959888ab7c5df460e8d06c9ef
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-30 14:50:25 -0300

    feat: configurando o bean

--- src/main/java/dev/_x/EventClean/infra/beans/BeanConfiguration.java
+++ src/main/java/dev/_x/EventClean/infra/beans/BeanConfiguration.java
@@ -1,4 +1,15 @@
 package dev._x.EventClean.infra.beans;
 
+import dev._x.EventClean.core.gateway.EventoGateway;
+import dev._x.EventClean.core.usecases.CriarEventoUsecase;
+import dev._x.EventClean.core.usecases.CriarEventoUsecaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BeanConfiguration {
+    @Bean
+    public CriarEventoUsecase criarEventoUsecase(EventoGateway eventoGateway){
+        return new CriarEventoUsecaseImpl(eventoGateway);
+    }
 }
