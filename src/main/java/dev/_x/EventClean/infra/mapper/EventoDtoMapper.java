a700a3d1de9f19a9bdd92d69ad511ade3266aa55
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-30 12:54:16 -0300

    feat: Adicionando o DTOMapper

--- src/main/java/dev/_x/EventClean/infra/mapper/EventoDtoMapper.java
+++ src/main/java/dev/_x/EventClean/infra/mapper/EventoDtoMapper.java
@@ -1,4 +1,40 @@
 package dev._x.EventClean.infra.mapper;
 
+import dev._x.EventClean.core.entities.Event;
+import dev._x.EventClean.infra.dtos.EventoDTO;
+import dev._x.EventClean.infra.persistence.EventEntity;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EventoDtoMapper {
+
+    public EventoDTO toDto(Event evento){
+        return new EventoDTO(
+                evento.id(),
+                evento.nome(),
+                evento.descricao(),
+                evento.identificador(),
+                evento.datainicio(),
+                evento.datafim(),
+                evento.localEvento(),
+                evento.capacidade(),
+                evento.organizador(),
+                evento.tipo()
+        );
+    }
+    public Event toDomain(EventoDTO eventoDTO){
+        return new Event(
+                eventoDTO.id(),
+                eventoDTO.nome(),
+                eventoDTO.descricao(),
+                eventoDTO.identificador(),
+                eventoDTO.datainicio(),
+                eventoDTO.datafim(),
+                eventoDTO.localEvento(),
+                eventoDTO.capacidade(),
+                eventoDTO.organizador(),
+                eventoDTO.tipo()
+                );
+    }
+
 }
