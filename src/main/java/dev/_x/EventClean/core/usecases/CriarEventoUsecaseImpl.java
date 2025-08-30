8342ec8b25bae6a12311d9b08027feccac1ed642
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-30 14:51:24 -0300

    feat: criando metodo de criar evento

--- src/main/java/dev/_x/EventClean/core/usecases/CriarEventoUsecaseImpl.java
+++ src/main/java/dev/_x/EventClean/core/usecases/CriarEventoUsecaseImpl.java
@@ -1,8 +1,16 @@
 package dev._x.EventClean.core.usecases;
 
 import dev._x.EventClean.core.entities.Event;
+import dev._x.EventClean.core.gateway.EventoGateway;
+
 
 public class CriarEventoUsecaseImpl implements CriarEventoUsecase {
+    private final EventoGateway eventoGateway;
+
+    public CriarEventoUsecaseImpl(EventoGateway eventoGateway) {
+        this.eventoGateway = eventoGateway;
+    }
+
     @Override
     public Event execute(Event evento) {
         return null;
--- src/main/java/dev/_x/EventClean/infra/gateway/EventoRepositoryGateway.java
+++ src/main/java/dev/_x/EventClean/infra/gateway/EventoRepositoryGateway.java
@@ -2,12 +2,21 @@ package dev._x.EventClean.infra.gateway;
 
 import dev._x.EventClean.core.entities.Event;
 import dev._x.EventClean.core.gateway.EventoGateway;
+import dev._x.EventClean.infra.mapper.EventoEntityMapper;
+import dev._x.EventClean.infra.persistence.EventEntity;
 import dev._x.EventClean.infra.persistence.EventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class EventoRepositoryGateway implements EventoGateway {
     private final EventRepository eventoRepository;
+    private final EventoEntityMapper mapper;
     @Override
     public Event criarEvento(Event evento) {
-        return eventoRepository.delete();
+        EventEntity entity = mapper.toEntity(evento);
+        EventEntity novoEvento = eventoRepository.save(entity);
+        return mapper.toDomain(novoEvento);
     }
 }
