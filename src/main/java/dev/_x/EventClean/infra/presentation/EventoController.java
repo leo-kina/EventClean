ed03277606815cc6eee26479a727a76da770068a
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-08-30 12:55:23 -0300

    feat: adicioando esqueleto da funcionalidade de criar evento

--- src/main/java/dev/_x/EventClean/infra/presentation/EventoController.java
+++ src/main/java/dev/_x/EventClean/infra/presentation/EventoController.java
@@ -1,4 +1,32 @@
 package dev._x.EventClean.infra.presentation;
 
-public class controller {
+import dev._x.EventClean.core.entities.Event;
+import dev._x.EventClean.core.usecases.CriarEventoUsecase;
+import dev._x.EventClean.infra.dtos.EventoDTO;
+import dev._x.EventClean.infra.mapper.EventoDtoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/")
+public class EventoController {
+
+    private final CriarEventoUsecase criarEventoUsecase;
+
+    private final EventoDtoMapper eventoDtoMapper;
+
+    public EventoController(CriarEventoUsecase criarEventoUsecase, EventoDtoMapper eventoDtoMapper) {
+        this.criarEventoUsecase = criarEventoUsecase;
+        this.eventoDtoMapper = eventoDtoMapper;
+    }
+
+    @PostMapping("/criarevento")
+    public EventoDTO criarEvento(@RequestBody EventoDTO eventoDto){
+        Event novoEvento = criarEventoUsecase.execute(eventoDtoMapper.toDomain(eventoDto));
+        return eventoDtoMapper.toDto(novoEvento);
+    }
+    @GetMapping
+    public String listadeevento(){
+        return "lista";
+    }
 }
